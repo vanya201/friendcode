ec168b7f2032027002d8d50f688aa2b19421e9e9
alumo <home89271@gmail.com>
2024-01-28 00:33:08 +0200

    ///

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <GLFW/glfw3.h>
+
+
+
+int main() {
+    // Initialize GLFW
+    if (!glfwInit()) {
+        return -1;
+    }
+
+    // Create a windowed mode window and its OpenGL context
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Triangle", NULL, NULL);
+
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        // Render here
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //// Draw the triangle
+        //drawTriangle();
+
+        //// Swap front and back buffers
+        //glfwSwapBuffers(window);
+
+        // Poll for and process events
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+
+    return 0;
+}
